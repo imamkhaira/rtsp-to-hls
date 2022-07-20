e112ed31ff6a3e4fdb084aaeb3fa88714413bfe6
Imam Khaira <imamkhaira@gmail.com>
2022-07-20 17:31:30 +0700

    fix: 🗿 rename part 2

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,5 @@
 import express from 'express';
-<<<<<<< HEAD
-import { WORK_DIRECTORY, OUTPUT_URL, PORT, STREAM_KEEPALIVE, RUNAS_UID } from './shared/config';
-=======
 import { WORK_DIRECTORY, OUTPUT_URL, PORT, STREAM_KEEPALIVE } from './shared/config';
->>>>>>> fix/docker-entrypoint
 import { StaticModule } from './modules/static/static.module';
 import { TranscoderModule } from './modules/transcoder/transcoder.module';
 
@@ -12,12 +8,7 @@ const App = express();
 const [transcoder, refresher] = TranscoderModule({
     workDir: WORK_DIRECTORY,
     outputUrl: OUTPUT_URL,
-<<<<<<< HEAD
-    keepalive: STREAM_KEEPALIVE,
-    userUid: RUNAS_UID
-=======
     keepalive: STREAM_KEEPALIVE
->>>>>>> fix/docker-entrypoint
 });
 
 App.use('/transcode', transcoder);
@@ -25,15 +16,5 @@ App.use(OUTPUT_URL, refresher);
 App.use(OUTPUT_URL, StaticModule(WORK_DIRECTORY));
 
 App.listen(PORT, () => {
-<<<<<<< HEAD
-    console.log(`server is listening on port ${PORT}`);
-    console.log(`config:`, {
-        workDir: WORK_DIRECTORY,
-        outputUrl: OUTPUT_URL,
-        keepalive: STREAM_KEEPALIVE,
-        userUid: RUNAS_UID
-    });
-=======
     console.table({ WORK_DIRECTORY, OUTPUT_URL, PORT, STREAM_KEEPALIVE });
->>>>>>> fix/docker-entrypoint
 });
